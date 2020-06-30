06dc66e24fd316168dbf8121c9ef4c38ae9e0b24
SimonSixt <sixtsim@gmail.com>
2020-06-30 08:49:42 +0200

    Magician fixed

--- A Castle Story/TutorialButton.java
+++ A Castle Story/TutorialButton.java
@@ -1,12 +1,7 @@
 import greenfoot.*;
 
 public class TutorialButton extends Buttons
-{
-    public TutorialButton()
-    {
-        setImage(new GreenfootImage("Tutorial", 96, Color.GREEN, new Color(0, 0, 0, 0)));
-    }
-    
+{   
     public void act() 
     {
         if(Greenfoot.mouseClicked(this))
